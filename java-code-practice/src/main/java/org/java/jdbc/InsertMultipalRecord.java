d90d8b2af29a9cc7817cd40f512c301345d7bf63
priyankabharti <priyankabharti.techdev@gmail.com>
2024-01-31 19:13:25 +0530

    Insert Multiples  Record

---
+++ java-code-practice/src/main/java/org/java/jdbc/InsertMultipalRecord.java
@@ -0,0 +1,55 @@
+package org.java.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class InsertMultipalRecord {
+    public static void main(String[] args) {
+        Connection con = null;
+        Statement stmt = null;
+        String qry1 = "insert into record.student values(102,'Piya','piya@gmail.com','Patna')";
+        String qry2 = "insert into record.student values(103,'Raaz','raaz@gmail.com','Patna')";
+        try{
+            // 1st step :- Loading the class And Registering
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver Class is Loaded and Registered...!!");
+
+            //2nd step :- Establish The Connection between Java Application to Database server
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306?user=root&password=root");
+            System.out.println("Connection Establish Successfully....!!");
+
+            //3rd step :- Create Platform
+            stmt = con.createStatement();
+            System.out.println("Platform Created Successfully..!!");
+
+            //4th step :- Execute The Sql Query
+            stmt.executeUpdate(qry1);
+            stmt.executeUpdate(qry2);
+
+            System.out.println("Recorded Added Successfully...!!");
+
+        }catch(ClassNotFoundException | SQLException exp){
+            exp.printStackTrace();
+        }
+        finally {
+            if(stmt != null){
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(con != null){
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("All Costly Resources Closed");
+        }
+
+    }
+}
