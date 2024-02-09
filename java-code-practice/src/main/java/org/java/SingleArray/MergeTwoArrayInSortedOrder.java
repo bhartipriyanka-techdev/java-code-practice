f3574caf8b28eaedcadf5c470ded13dcc05e8e48
priyankabharti <priyankabharti.techdev@gmail.com>
2024-02-09 19:13:02 +0530

    Merge Two Array in Sorted Order

--- java-code-practice/src/main/java/org/java/SingleArray/MergeTwoArrayInSortedOrder.java
+++ java-code-practice/src/main/java/org/java/SingleArray/MergeTwoArrayInSortedOrder.java
@@ -1,23 +1,18 @@
 package org.java.SingleArray;
-
 import java.util.Scanner;
 //array1 => {23,34,77,85}
 //array2 => {12 28 29 56 88 99}
 //OUTPut =>{12 23 23 29 34 56 77 77 85 85}
-
 public class MergeTwoArrayInSortedOrder {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter Size of 1st Array :");
         int size1 = scanner.nextInt();
         int arr1[] = new int[size1];
-
         System.out.println("Enter data in 1st array in sorted order:");
         for (int i = 0; i < arr1.length; i++) {
             arr1[i] = scanner.nextInt();
         }
-
         System.out.print("Enter Size of 2nd Array :");
         int size2 = scanner.nextInt();
         int arr2[] = new int[size2];
@@ -26,17 +21,14 @@ public class MergeTwoArrayInSortedOrder {
         for (int i = 0; i < arr2.length; i++) {
             arr2[i] = scanner.nextInt();
         }
-
         System.out.println("-------------1st Array---------------");
         for (int data : arr1) {
             System.out.print(data + " ");
         }
-
         System.out.println("\n-------------2nd Array---------------");
         for (int data : arr2) {
             System.out.print(data + " ");
         }
-
         int arr3[] = new int[arr1.length + arr2.length];
         int i = 0, j = 0, k = 0;
         while (i < arr1.length && j < arr2.length) {
@@ -49,7 +41,6 @@ public class MergeTwoArrayInSortedOrder {
             }
             k++;
         }
-
         // Copy the remaining elements from arr1 (if any)
         while (i < arr1.length) {
             arr3[k] = arr1[i];
